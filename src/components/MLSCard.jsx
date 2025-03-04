7a88224c400c5beb091d53ef1f7fb55fa1f6005a
Ahtesham Akram <ahte02254@gmail.com>
2025-03-04 16:31:36 +0500

    Update MLSCard.jsx

--- src/components/MLSCard.jsx
+++ src/components/MLSCard.jsx
@@ -5,19 +5,19 @@ import DEFAULT_HOME from "./../assets/img/default-img.png"
 const MLSCard = ({ property }) => {
     return (
         <Card className="property-card mb-3 mx-1">
-            <Card.Img variant="top" src={property.DefaultImage ? property.DefaultImage : DEFAULT_HOME} alt="Property Image" />
+            <Card.Img variant="top" src={property?.DefaultImage ? property?.DefaultImage : DEFAULT_HOME} alt="Property Image" />
             <Card.Body>
-                <Card.Title>${property.ListPrice.toLocaleString()}</Card.Title>
+                <Card.Title>${property?.ListPrice?.toLocaleString()}</Card.Title>
                 <Card.Text>
-                    <strong>{property.TotalBedrooms}</strong> Beds | <strong>{property.TotalBaths}</strong> Baths | <strong>{property.LivingSquareFeet}</strong> sqft
+                    <strong>{property?.TotalBedrooms}</strong> Beds | <strong>{property?.TotalBaths}</strong> Baths | <strong>{property?.LivingSquareFeet}</strong> sqft
                 </Card.Text>
                 <Card.Text>
-                    📍 {property.FullStreetAddress}, {property.CityName}, {property.StateCode}
+                    📍 {property?.FullStreetAddress}, {property?.CityName}, {property?.StateCode}
                 </Card.Text>
                 <Card.Text>
-                    🏡 Type: {property.PropertyTypeString}
+                    🏡 Type: {property?.PropertyTypeString}
                 </Card.Text>
-                <Button href={property.CustomUrl} target="_blank" className="text-white fw-600 bg-main-clr border-0 mt-3">
+                <Button href={property?.CustomUrl} target="_blank" className="text-white fw-600 bg-main-clr border-0 mt-3">
                     View Details
                 </Button>
             </Card.Body>
