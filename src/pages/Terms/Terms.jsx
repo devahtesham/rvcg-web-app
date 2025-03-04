dfb7684afdaf234765d3a9b5f28c712ff145a065
devahtesham <ahte02254@gmail.com>
2025-03-05 00:51:30 +0500

    terms page has been completed

--- src/pages/Terms/Terms.jsx
+++ src/pages/Terms/Terms.jsx
@@ -2,7 +2,79 @@ import React from 'react'
 
 const Terms = () => {
   return (
-    <div>Terms</div>
+    <div className='container'>
+      <section className='mt-5'>
+        <h1 className='mb-4'>Terms & Conditions</h1>
+        <div className='mb-3'>
+          <h6>Legal Consultation, Disclosure, and Compliance Agreement:</h6>
+          <p className='mb-3'>
+            This Legal Consultation, Disclosure, and Compliance Agreement (“Agreement”) is entered into between RVCG First (“Company”) and clients, including buyers, sellers, and investors (“Clients”).
+          </p>
+        </div>
+        <div className='mb-3'>
+          <h6>Avoiding Agency Representation::</h6>
+          <p className='mb-3'>
+            The Company is committed to avoiding agency representation in real estate transactions. Clients understand that the Company does not act as an agent or representative in real estate deals. The Company’s role is to facilitate introductions, provide information, and offer referral services. Clients should seek independent legal advice to clarify their roles and responsibilities to avoid unintentional agency relationships.
+          </p>
+        </div>
+        <div className='mb-3'>
+          <h6>Referral Activities as a Wholesaler or Investor:</h6>
+          <p className='mb-3'>
+            Clients may be referred to licensed real estate agents, brokers, or wholesalers who offer real estate services. Additionally, the Company may operate as a wholesaler or investor in certain transactions. Clients acknowledge that agreements with referred parties or transactions with the Company as a wholesaler or investor are independent of the Company’s referral services. All referral agreement terms, including compensation, will be disclosed in writing to the Client before referrals are made.
+          </p>
+        </div>
+        <div className='mb-3'>
+          <h6>Referral Commission Standards in the Real Estate Industry</h6>
+          <p className='mb-3'>
+            We are committed to transparency, fairness, and integrity in all aspects of our business, including our referral commission structure. When establishing our commission rates, we carefully consider industry standards and best practices to ensure that our referral program provides value to both referrers and clients. Here’s how we approach setting our referral commission standards:
+          </p>
+        </div>
+        <section className="points mt-3">
+          <p className='mb-3'>
+            <p className='fw-bold d-inline'>1. Industry Standards:</p> We conduct thorough research to understand what other companies in the real estate industry are offering for referral commissions. This allows us to gain insight into common practices and benchmark our commission structure accordingly.
+          </p>
+          <p className='mb-3'>
+            <p className='fw-bold d-inline'>2. Value of the Referral:</p> We recognize the significant impact that a successful referral can have on our business. We consider the lifetime value of a referred client, taking into account factors such as potential repeat business and long-term relationships. If a successful referral leads to ongoing or repeat business that generates significant revenue, we may offer a higher commission percentage to reflect the value of the referral.
+          </p>
+          <p className='mb-3'>
+            <p className='fw-bold d-inline'>3. Cost of Acquisition:</p> We carefully evaluate the cost of acquiring a new client through various marketing channels, comparing it to the cost of paying referral commissions. If paying a certain commission percentage results in a lower cost per acquisition compared to other marketing efforts, we consider it a fair deal for all parties involved
+          </p>
+          <p className='mb-3'>
+            <p className='fw-bold d-inline'>4. Competitiveness:</p> We assess the competitiveness of our commission structure to ensure that it incentivizes individuals to make referrals. If the commission percentage is too low compared to competitors, it may not be attractive enough to encourage active participation in our referral program. We strive to offer competitive commission rates that motivate referrers to actively promote our services.
+          </p>
+          <p className='mb-3'>
+            <p className='fw-bold d-inline'>5. Transparency and Trust:</p> We believe in fostering transparency and trust in our referral program. We ensure that our commission structure is clearly communicated to referrers and clients, providing full transparency regarding how commissions are calculated and paid. Transparency builds trust and encourages participation in our referral program, strengthening our relationships with clients and partners.
+          </p>
+          <p className='mb-3'>
+            <p className='fw-bold d-inline'>Disclaimer:</p> While we make every effort to ensure the fairness and accuracy of our referral commission structure, Realty Vision Capital Group reserves the right to modify, suspend, or terminate the referral program at any time without prior notice. This statement is provided for informational purposes only and does not constitute legal advice. By participating in our referral program, you agree to abide by the terms and conditions outlined by Realty Vision Capital Group
+          </p>
+
+        </section>
+
+        <div className='mb-3'>
+          <h6>Company’s Agent Representation:</h6>
+          <p className='mb-3'>
+            The Company may engage licensed real estate agents to represent the Company in real estate transactions. Clients acknowledge that these agents operate under the Company’s banner. These agents are not employees of the Company but collaborate independently. Clients should conduct due diligence when working with Company-affiliated agents and verify their credentials.
+          </p>
+        </div>
+        <div className='mb-3'>
+          <h6>Disclosure and Compliance:</h6>
+          <p className='mb-3'>
+            The Company operates in strict compliance with federal, state, and local real estate laws and regulations. Clients acknowledge that the Company is not a licensed real estate broker or agent and is not engaged in real estate agency activities.
+          </p>
+        </div>
+        <div className='mb-3'>
+          <h6>Limitation of Liability:</h6>
+          <p className='mb-3'>
+            Clients agree that the Company, including its agents, is not liable for actions, omissions, or disputes arising from transactions with referred parties or the Company’s activities as a wholesaler or investor. The Company’s liability is limited to the referral services provided. By utilizing the Company’s services and website, Clients confirm that they have read, comprehended, and accepted the terms of this Agreement.
+          </p>
+        </div>
+        <p>
+        RVCG First
+        </p>
+
+      </section>
+    </div>
   )
 }
 
